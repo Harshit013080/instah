a75a54e1bda880f7044d6038a847f57f097b6051
1308harshit <khatsuriyaharshit@gmail.com>
2025-11-30 22:56:25 +0530

    docker file

--- backend/scraper/browser.js
+++ backend/scraper/browser.js
@@ -8,19 +8,12 @@ export async function launchBrowser() {
       '--disable-dev-shm-usage', // Overcome limited resource problems
       '--no-sandbox',
       '--disable-setuid-sandbox',
-      '--disable-gpu', // Disable GPU acceleration
+      '--disable-gpu', // Faster on headless
       '--disable-extensions', // Faster startup
       '--disable-background-networking', // Faster
       '--disable-background-timer-throttling',
       '--disable-renderer-backgrounding',
       '--disable-backgrounding-occluded-windows',
-      // Force software rendering (no GPU/GBM required) - for Render/Railway deployment
-      '--use-gl=swiftshader',
-      '--disable-webgl',
-      '--disable-3d-apis',
-      // Additional flags for containerized environments
-      '--disable-features=UseChromeOSDirectVideoDecoder',
-      '--disable-features=VaapiVideoDecoder',
     ],
   });
 }
